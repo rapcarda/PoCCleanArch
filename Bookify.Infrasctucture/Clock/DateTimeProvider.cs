c733565165658f6f7f4c65d319f6bb3a3f537094
Rafael Carda <rapcarda@gmail.com>
2024-01-04 17:26:03 -0300

    Criando Infrastucture e adicoinando serviços DateTimeProvider e EmailService

---
+++ Bookify.Infrasctucture/Clock/DateTimeProvider.cs
@@ -0,0 +1,7 @@
+﻿using Bookify.Application.Abstractions.Clock;
+
+namespace Bookify.Infrasctucture.Clock;
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
---
+++ Bookify.Infrasctucture/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
+using Bookify.Infrasctucture.Clock;
+using Bookify.Infrasctucture.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Infrasctucture;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        services.AddTransient<IEmailService, EmailService>();
+        
+        return services;
+    }
+}
