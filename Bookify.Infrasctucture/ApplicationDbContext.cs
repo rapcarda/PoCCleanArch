f74d04508da022e4569fc155e4297c9dc6b399d0
Rafael Carda <rapcarda@gmail.com>
2024-01-04 18:01:09 -0300

    Adicionando e configurando EF Core

---
+++ Bookify.Infrasctucture/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrasctucture;
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
--- Bookify.Infrasctucture/DependencyInjection.cs
+++ Bookify.Infrasctucture/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Bookify.Application.Abstractions.Email;
 using Bookify.Infrasctucture.Clock;
 using Bookify.Infrasctucture.Email;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +13,16 @@ public static class DependencyInjection
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
         services.AddTransient<IEmailService, EmailService>();
+
+        var connectionString = configuration.GetConnectionString("DataBase") ??
+            throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            // O EF Core por convenção usa TitleCase para nomes de tabelas e colunas, mas o postgre usa SnakeCase.
+            // Para facilitar a conversão, instalar o package EFCore.NameingConventions e utilizar o método UseSnakeCaseNamingConvention()
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+        });
         
         return services;
     }
