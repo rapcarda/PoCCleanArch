87deb2b16a12bf7f81f279ab6da3bd839ef3876c
Rafael Carda <rapcarda@gmail.com>
2024-01-09 17:58:27 -0300

    Adicionando configuração para apply automaticamente as configs

--- Bookify.Infrasctucture/ApplicationDbContext.cs
+++ Bookify.Infrasctucture/ApplicationDbContext.cs
@@ -7,4 +7,10 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
